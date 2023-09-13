d0795c47d4afebfa0c759daec0d112327efd503b
天天 <tiantian@tiantiandeMacBook-Air.local>
2023-09-13 14:38:30 -0400

    refactored to extract helper method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 0;
+        int n = 100;
+        while (n > 0) {
+            boolean divisibleBy2 = n % 2 == 0;
+            if (divisibleBy2) {
+                n /= 2;
+            } else {
+                n = n - 1;
+
+            }
+            i++;
+
+
+        }
+        System.out.println(i);
+    }
 }
